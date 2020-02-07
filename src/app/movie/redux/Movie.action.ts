9d621941e654bf85327a88b1a03527a728b5077f
Kacper Starzyński <Orwas4@wp.pl>
2020-02-07 23:21:29 +0100

    Apply Inversion of Control to make MoviePanel component testable

--- src/app/movie/redux/Movie.action.ts
+++ src/app/movie/redux/Movie.action.ts
@@ -1,16 +1,15 @@
 import { MovieDTO } from "../data/Movie.dto";
 import { RetrieveMovieListAction, MovieListRetrieved, ClearMovieListAction, ClearMovieListRequest } from "./MovieListActions.type";
-import { getAllMoviesREST } from "../data/Movie.service";
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { AppState } from "../../../common/redux/store/ConfigureStore";
 import { AppActions } from "../../../common/redux/actions/AppActions.type";
 
-export const retrieveMoviesAction = (): ThunkAction<Promise<void>,
+export const retrieveMoviesAction = (getAllMovies: () => Promise<MovieDTO[]>): ThunkAction<Promise<void>,
     AppState,
     undefined,
     AppActions
 > => async (dispatch: ThunkDispatch<AppState, undefined, AppActions>) => {
-    const movies: MovieDTO[] = await getAllMoviesREST();
+    const movies: MovieDTO[] = await getAllMovies();
     dispatch(moviesRetrieved(movies));
 };
 
