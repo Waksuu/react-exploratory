369131ddac67e5ccb9ef66734989debaf995edc9
Kacper Starzyński <Orwas4@wp.pl>
2020-02-08 00:23:43 +0100

    Remove concrete implementation of getAllMovies from Movie.action

--- src/app/movie/redux/Movie.action.ts
+++ src/app/movie/redux/Movie.action.ts
@@ -1,13 +1,12 @@
 import { MovieDTO } from "../data/Movie.dto";
 import { RetrieveMovieListAction, MovieListRetrieved, ClearMovieListAction, ClearMovieListRequest } from "./MovieListActions.type";
-import { getAllMoviesREST } from "../data/Movie.service";
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { AppState } from "../../../common/redux/store/ConfigureStore";
 import { AppActions } from "../../../common/redux/actions/AppActions.type";
 
 export const retrieveMoviesAction = (getAllMovies: () => Promise<MovieDTO[]>): ThunkAction<Promise<void>, AppState, undefined, AppActions> => {
     return async (dispatch: ThunkDispatch<AppState, any, AppActions>) => {
-        const movies: MovieDTO[] = await getAllMoviesREST();
+        const movies: MovieDTO[] = await getAllMovies();
         dispatch(moviesRetrieved(movies));
     };
 };
